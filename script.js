7c102c6531b3598941f26770d94c947b85813df9
mhansi <srsmsone@gmail.com>
2019-05-08 23:50:47 +0530

    Created Repo

---
+++ script.js
@@ -0,0 +1,12 @@
+
+document.getElementById('lbsInput').addEventListener('input',function(e){
+
+    
+    var lbs =e.target.value;
+    document.getElementById('gramsOutput').innerHTML=lbs/0.0022046;
+
+    document.getElementById('kgOutput').innerHTML=lbs/2.2046;
+   
+    document.getElementById('ozOutput').innerHTML=lbs*16;
+    
+ });
