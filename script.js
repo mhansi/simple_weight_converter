dfaaf55e5e26bbb85451be6dbb9cb650f1cb4bd9
Charunirathnayake <38881962+Charunirathnayake@users.noreply.github.com>
2019-10-10 14:11:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,13 @@ document.getElementById('lbsInput').addEventListener('input',function(e){
     document.getElementById('ozOutput').innerHTML=lbs*16;
     
  });
+document.getElementById('heightinput').addEventListener('input',function(e){
+	var hbs=e.target.value;
+	document.getElementById('metersoutput').innerHTML=hbs/3.28;
+
+	document.getElementById('centimeteroutput').innerHTML=hbs/30.48;
+
+	document.getElementById('decimeteroutput').innerHTML=hbs/3.048;
+}
+
+	);
